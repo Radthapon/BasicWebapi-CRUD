223a5f0c0871d8b08ece0e7080cb20c17d8f92de
Dev-Radthpon.JS <radthapon747628@gmail.com>
2023-09-15 15:02:30 +0700

    test commit

---
+++ BasicWebapi-CRUD/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicWebapi_CRUD.Controllers
+{
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string Greeting ()
+        {
+            return "Hi";
+        }
+
+        //CRUD
+        //Create - new record => [POST]
+        //Read - retrieve a single and list of record => [GET]
+        //Update - modify and existing id record => [PUT]
+        //Delete - remove and existing id record => [DELETE]
+
+        // HTTP Verbs
+        // GET
+        // POST
+        // PUT or PATCH
+        // DELETE
+    }
+}
